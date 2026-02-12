48d539bf00582a74fee1078b714109499409b738
Fady Awan <fadyawan@Fadys-MacBook-Pro.local>
2026-02-12 11:43:01 +0500

    changing package id

--- lib/common/widgets/custom_app_bar_widget.dart
+++ lib/common/widgets/custom_app_bar_widget.dart
@@ -1,16 +1,140 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_restaurant/helper/responsive_helper.dart';
+// import 'package:flutter_restaurant/main.dart';
+// import 'package:flutter_restaurant/utill/dimensions.dart';
+// import 'package:go_router/go_router.dart';
+
+// class CustomAppBarWidget extends StatelessWidget
+//     implements PreferredSizeWidget {
+//   final String? title;
+//     final BuildContext? context;
+
+//   final bool isBackButtonExist;
+//   final Function? onBackPressed;
+//   final Widget? actionView;
+//   final bool centerTitle;
+//   final bool isTransparent;
+//   final double elevation;
+//   final Widget? leading;
+//   final Color? titleColor;
+
+//   const CustomAppBarWidget({
+//     super.key,
+//     required this.title,
+//     this.isBackButtonExist = true,
+//     this.onBackPressed,
+//     this.actionView,    this.context,
+
+//     this.centerTitle = true,
+//     this.isTransparent = false,
+//     this.elevation = 0,
+//     this.leading,
+//     this.titleColor,
+//   });
+
+//   void _handleBack(BuildContext context) {
+//     if (onBackPressed != null) {
+//       onBackPressed!();
+//     } else if (context.canPop()) {
+//       context.pop();
+//     } else {
+//       // Optional fallback (change '/' if your home route is different)
+//       context.go('/');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       elevation: elevation,
+//       centerTitle: centerTitle,
+//       backgroundColor:
+//           isTransparent ? Colors.transparent : Theme.of(context).primaryColor,
+//       title: FittedBox(
+//         child: Stack(
+//           children: [
+//             // Border text
+//             Text(
+//               title ?? '',
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 fontFamily: 'Pacifico',
+//                 fontSize: 18,
+//                 height: 1.2,
+//                 letterSpacing: 1.5,
+//                 foreground: Paint()
+//                   ..style = PaintingStyle.stroke
+//                   ..strokeWidth = 3
+//                   ..color = Theme.of(context).primaryColor,
+//               ),
+//             ),
+//             // Fill text
+//             Text(
+//               title ?? '',
+//               textAlign: TextAlign.center,
+//               style: const TextStyle(
+//                 fontFamily: 'Pacifico',
+//                 fontSize: 18,
+//                 height: 1.2,
+//                 letterSpacing: 1.5,
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       leading: isBackButtonExist
+//           ? (leading != null
+//               ? GestureDetector(
+//                   onTap: () => _handleBack(context),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: leading,
+//                   ),
+//                 )
+//               : IconButton(
+//                   icon: const Icon(Icons.arrow_back_ios),
+//                   color: titleColor ?? Colors.red,
+//                   onPressed: () => _handleBack(context),
+//                 ))
+//           : const SizedBox(),
+//       actions: actionView != null
+//           ? [
+//               Padding(
+//                 padding:
+//                     const EdgeInsets.only(right: Dimensions.paddingSizeSmall),
+//                 child: actionView!,
+//               )
+//             ]
+//           : [],
+//     );
+//   }
+
+//   @override
+//   Size get preferredSize => Size(
+//         double.maxFinite,
+//         ResponsiveHelper.isDesktop(Get.context) ? 100 : 50,
+//       );
+// }
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant/helper/responsive_helper.dart';
 import 'package:flutter_restaurant/main.dart';
 import 'package:flutter_restaurant/utill/dimensions.dart';
+import 'package:flutter_restaurant/utill/images.dart';
+import 'package:flutter_restaurant/utill/styles.dart';
 import 'package:go_router/go_router.dart';
 
-class CustomAppBarWidget extends StatelessWidget
-    implements PreferredSizeWidget {
+class CustomAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   final String? title;
-    final BuildContext? context;
-
   final bool isBackButtonExist;
   final Function? onBackPressed;
+  final BuildContext? context;
   final Widget? actionView;
   final bool centerTitle;
   final bool isTransparent;
@@ -23,8 +147,8 @@ class CustomAppBarWidget extends StatelessWidget
     required this.title,
     this.isBackButtonExist = true,
     this.onBackPressed,
-    this.actionView,    this.context,
-
+    this.context,
+    this.actionView,
     this.centerTitle = true,
     this.isTransparent = false,
     this.elevation = 0,
@@ -32,30 +156,15 @@ class CustomAppBarWidget extends StatelessWidget
     this.titleColor,
   });
 
-  void _handleBack(BuildContext context) {
-    if (onBackPressed != null) {
-      onBackPressed!();
-    } else if (context.canPop()) {
-      context.pop();
-    } else {
-      // Optional fallback (change '/' if your home route is different)
-      context.go('/');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      elevation: elevation,
-      centerTitle: centerTitle,
-      backgroundColor:
-          isTransparent ? Colors.transparent : Theme.of(context).primaryColor,
       title: FittedBox(
         child: Stack(
           children: [
-            // Border text
+            // BORDER
             Text(
-              title ?? '',
+              title!,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontFamily: 'Pacifico',
@@ -68,9 +177,10 @@ class CustomAppBarWidget extends StatelessWidget
                   ..color = Theme.of(context).primaryColor,
               ),
             ),
-            // Fill text
+
+            // FILL
             Text(
-              title ?? '',
+              title!,
               textAlign: TextAlign.center,
               style: const TextStyle(
                 fontFamily: 'Pacifico',
@@ -83,21 +193,48 @@ class CustomAppBarWidget extends StatelessWidget
           ],
         ),
       ),
+      centerTitle: centerTitle,
       leading: isBackButtonExist
-          ? (leading != null
-              ? GestureDetector(
-                  onTap: () => _handleBack(context),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: leading,
-                  ),
-                )
-              : IconButton(
-                  icon: const Icon(Icons.arrow_back_ios),
-                  color: titleColor ?? Colors.red,
-                  onPressed: () => _handleBack(context),
-                ))
-          : const SizedBox(),
+    ? (leading != null
+        ? GestureDetector(
+            onTap: () {
+              if (onBackPressed != null) {
+                onBackPressed!();
+              } else if (context.canPop()) {
+                context.pop();
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: leading,
+            ),
+          )
+        : IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            color: titleColor ?? Colors.red,
+            onPressed: () {
+              if (onBackPressed != null) {
+                onBackPressed!();
+              } else if (context.canPop()) {
+                context.pop();
+              }
+            },
+          ))
+    : (leading != null
+        ? GestureDetector(
+            onTap: () {
+              if (onBackPressed != null) {
+                onBackPressed!();
+              } else if (context.canPop()) {
+                context.pop();
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: leading,
+            ),
+          )
+        : const SizedBox()),
       actions: actionView != null
           ? [
               Padding(
@@ -107,6 +244,9 @@ class CustomAppBarWidget extends StatelessWidget
               )
             ]
           : [],
+      // backgroundColor:
+      // isTransparent ? Colors.transparent : Theme.of(context).primaryColor,
+      elevation: elevation,
     );
   }
 
